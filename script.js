83b30c855e30b032c9f04a180790e559c62da164
Chloe <chloeh24@hotmail.com>
2020-01-20 14:45:23 +0900

    edit buttons, add clear and delete functions

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-function operate(num1, num2, operator){
-    return num1 operator num2
+var display = document.querySelector('#displayText')
+var buttonArr = document.querySelectorAll('.calc')
+var displayValue = '';
 
-}
+//display calculations
+for (var i=0; i< buttonArr.length; i++){
+    buttonArr[i].addEventListener('click', function(){
+        displayValue += this.innerHTML;
+        display.innerHTML = displayValue;
+    })
+};
+
+//do calculations
+
+
+
+//clear button
+
+var clear = document.querySelector('#clear')
+
+clear.addEventListener('click', function(){
+    displayValue = ''
+    display.innerHTML = displayValue;
+})
+
+//delete button 
+var del = document.querySelector('#delete')
+
+del.addEventListener('click', function(){
+    displayValue = displayValue.slice(0, displayValue.length-1);
+    display.innerHTML = displayValue;
+});
